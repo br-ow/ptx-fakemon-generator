2cddf1059b8b4324313e339d43a7d68a478ca79a
Brittany Owens <me@brittany.tech>
2017-08-18 16:12:38 -0400

    TypeList now contains an actual list of types (just a placeholder pre-written one for now) and generates pseudorandom primary types. Refactoring: Added "areAllIdentical" testing function so I don't have to keep writing 10 separate comparisons each time.

--- src/Type/TypeList.java
+++ src/Type/TypeList.java
@@ -6,6 +6,10 @@
 
 package Type;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Singleton list of all available types
  * Also serves as a factory object to create Typings
@@ -14,10 +18,23 @@ package Type;
 public class TypeList {
     
     private static TypeList instance;
+    private static ArrayList<String> all_types; //The actual list of types that we are generating from
+    private final Random rand;
     
     //Private Constructor (necessary for singleton)
     private TypeList() {
+        //TODO: read the list of types from an external config file or something
+        //For now, here is a list of some placeholder types
+        all_types = new ArrayList<>();
+        all_types.add("Fire");
+        all_types.add("Water");
+        all_types.add("Grass");
+        all_types.add("Normal");
+        all_types.add("Bug");
+        all_types.add("Flying");
         
+        rand = new Random(); //For random number generation
+        //Using Random() instead of ThreadLocalRandom for ease of testing later.
     }
     
     //If instance doesn't exist, create it. Either way, return it.
@@ -30,6 +47,10 @@ public class TypeList {
     
     //Create a Typing object with random types
     public Typing genRandomTyping() {
-        return new Typing("Boof");
+        //Generate a primary type; equal chance of any type on the list
+        int which_primary = rand.nextInt(all_types.size());
+        
+        //Pass the generated primary type to the Typing constructor and return it
+        return new Typing(all_types.get(which_primary));
     }
 }
--- test/fakemon_generator_tests/PokeTest.java
+++ test/fakemon_generator_tests/PokeTest.java
@@ -6,6 +6,10 @@ package fakemon_generator_tests;
  * and open the template in the editor.
  */
 
+import Type.TypeList;
+import Type.Typing;
+import java.util.ArrayList;
+import java.util.ListIterator;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
@@ -13,8 +17,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import ptu_fakemon_generator.Pokemon;
-import Type.TypeList;
-import Type.Typing;
 
 /**
  *
@@ -22,7 +24,24 @@ import Type.Typing;
  */
 public class PokeTest {
     
-    
+        //for comparing many of the same sort of object
+        //ONLY FOR USE on lists of items that implement the equals() function!
+        //So ONLY use it for primitives, strings, etc. and not the classes implemented in this project.
+    private boolean areAllIdentical(ArrayList list) {
+            boolean answer = true;
+            //Start the iterator at the second thing on the list
+            //We don't need to compare the first item against itself
+            ListIterator iterator = list.listIterator(1); 
+            while (iterator.hasNext()) {
+                if ( (list.get(0).equals(iterator.next())) ) {
+                    //do nothing, because these two are identical
+                }
+                else { //these two are not identical
+                    answer = false;
+                }
+            }
+            return answer;
+    };
     
     @BeforeClass
     public static void setUpClass() {
@@ -129,17 +148,20 @@ public class PokeTest {
         podmon.setBST(45);
         podmon.distrStats();
         
-        assertTrue(
-            (pokmon.getHp() != pokermon.getHp()) ||
-            (pokmon.getHp() != pokomon.getHp()) ||
-            (pokmon.getHp() != poqmon.getHp()) ||
-            (pokmon.getHp() != pocmon.getHp()) ||
-            (pokmon.getHp() != pokamon.getHp()) ||
-            (pokmon.getHp() != pokumon.getHp()) ||
-            (pokmon.getHp() != pokymon.getHp()) ||
-            (pokmon.getHp() != popmon.getHp()) ||
-            (pokmon.getHp() != podmon.getHp())
-        );
+        //Add all of the generated HP values to a list so that we can more easily compare them
+        ArrayList pokelist = new ArrayList ();
+        pokelist.add(pokmon.getHp());
+        pokelist.add(pokermon.getHp());
+        pokelist.add(pokomon.getHp());
+        pokelist.add(poqmon.getHp());
+        pokelist.add(pocmon.getHp());
+        pokelist.add(pokamon.getHp());
+        pokelist.add(pokumon.getHp());
+        pokelist.add(pokymon.getHp());
+        pokelist.add(popmon.getHp());
+        pokelist.add(podmon.getHp());
+        
+        assertFalse(areAllIdentical(pokelist));
     }
     
     //Test that the stats are not the same every time
@@ -185,17 +207,20 @@ public class PokeTest {
         podmon.setBST(45);
         podmon.distrStats();
         
-        assertTrue(
-            (pokmon.getAtk() != pokermon.getAtk()) ||
-            (pokmon.getAtk() != pokomon.getAtk()) ||
-            (pokmon.getAtk() != poqmon.getAtk()) ||
-            (pokmon.getAtk() != pocmon.getAtk()) ||
-            (pokmon.getAtk() != pokamon.getAtk()) ||
-            (pokmon.getAtk() != pokumon.getAtk()) ||
-            (pokmon.getAtk() != pokymon.getAtk()) ||
-            (pokmon.getAtk() != popmon.getAtk()) ||
-            (pokmon.getAtk() != podmon.getAtk())
-        );
+        //Add all of the generated ATK values to a list so that we can more easily compare them
+        ArrayList pokelist = new ArrayList ();
+        pokelist.add(pokmon.getAtk());
+        pokelist.add(pokermon.getAtk());
+        pokelist.add(pokomon.getAtk());
+        pokelist.add(poqmon.getAtk());
+        pokelist.add(pocmon.getAtk());
+        pokelist.add(pokamon.getAtk());
+        pokelist.add(pokumon.getAtk());
+        pokelist.add(pokymon.getAtk());
+        pokelist.add(popmon.getAtk());
+        pokelist.add(podmon.getAtk());
+        
+        assertFalse(areAllIdentical(pokelist));
     }
     
     //Test that the stats are not the same every time
@@ -241,17 +266,21 @@ public class PokeTest {
         podmon.setBST(45);
         podmon.distrStats();
         
-        assertTrue(
-            (pokmon.getDef() != pokermon.getDef()) ||
-            (pokmon.getDef() != pokomon.getDef()) ||
-            (pokmon.getDef() != poqmon.getDef()) ||
-            (pokmon.getDef() != pocmon.getDef()) ||
-            (pokmon.getDef() != pokamon.getDef()) ||
-            (pokmon.getDef() != pokumon.getDef()) ||
-            (pokmon.getDef() != pokymon.getDef()) ||
-            (pokmon.getDef() != popmon.getDef()) ||
-            (pokmon.getDef() != podmon.getDef())
-        );
+        //Add all of the generated DEF values to a list so that we can more easily compare them
+        ArrayList pokelist = new ArrayList ();
+        pokelist.add(pokmon.getDef());
+        pokelist.add(pokermon.getDef());
+        pokelist.add(pokomon.getDef());
+        pokelist.add(poqmon.getDef());
+        pokelist.add(pocmon.getDef());
+        pokelist.add(pokamon.getDef());
+        pokelist.add(pokumon.getDef());
+        pokelist.add(pokymon.getDef());
+        pokelist.add(popmon.getDef());
+        pokelist.add(podmon.getDef());
+        
+        assertFalse(areAllIdentical(pokelist));
+        
     }
     
     //Test that the stats are not the same every time
@@ -297,17 +326,20 @@ public class PokeTest {
         podmon.setBST(45);
         podmon.distrStats();
         
-        assertTrue(
-            (pokmon.getSpAtk() != pokermon.getSpAtk()) ||
-            (pokmon.getSpAtk() != pokomon.getSpAtk()) ||
-            (pokmon.getSpAtk() != poqmon.getSpAtk()) ||
-            (pokmon.getSpAtk() != pocmon.getSpAtk()) ||
-            (pokmon.getSpAtk() != pokamon.getSpAtk()) ||
-            (pokmon.getSpAtk() != pokumon.getSpAtk()) ||
-            (pokmon.getSpAtk() != pokymon.getSpAtk()) ||
-            (pokmon.getSpAtk() != popmon.getSpAtk()) ||
-            (pokmon.getSpAtk() != podmon.getSpAtk())
-        );
+        //Add all of the generated SPATK values to a list so that we can more easily compare them
+        ArrayList pokelist = new ArrayList ();
+        pokelist.add(pokmon.getSpAtk());
+        pokelist.add(pokermon.getSpAtk());
+        pokelist.add(pokomon.getSpAtk());
+        pokelist.add(poqmon.getSpAtk());
+        pokelist.add(pocmon.getSpAtk());
+        pokelist.add(pokamon.getSpAtk());
+        pokelist.add(pokumon.getSpAtk());
+        pokelist.add(pokymon.getSpAtk());
+        pokelist.add(popmon.getSpAtk());
+        pokelist.add(podmon.getSpAtk());
+        
+        assertFalse(areAllIdentical(pokelist));
     }
     
     //Test that the stats are not the same every time
@@ -353,17 +385,20 @@ public class PokeTest {
         podmon.setBST(45);
         podmon.distrStats();
         
-        assertTrue(
-            (pokmon.getSpDef() != pokermon.getSpDef()) ||
-            (pokmon.getSpDef() != pokomon.getSpDef()) ||
-            (pokmon.getSpDef() != poqmon.getSpDef()) ||
-            (pokmon.getSpDef() != pocmon.getSpDef()) ||
-            (pokmon.getSpDef() != pokamon.getSpDef()) ||
-            (pokmon.getSpDef() != pokumon.getSpDef()) ||
-            (pokmon.getSpDef() != pokymon.getSpDef()) ||
-            (pokmon.getSpDef() != popmon.getSpDef()) ||
-            (pokmon.getSpDef() != podmon.getSpDef())
-        );
+        //Add all of the generated SPDEF values to a list so that we can more easily compare them
+        ArrayList pokelist = new ArrayList ();
+        pokelist.add(pokmon.getSpDef());
+        pokelist.add(pokermon.getSpDef());
+        pokelist.add(pokomon.getSpDef());
+        pokelist.add(poqmon.getSpDef());
+        pokelist.add(pocmon.getSpDef());
+        pokelist.add(pokamon.getSpDef());
+        pokelist.add(pokumon.getSpDef());
+        pokelist.add(pokymon.getSpDef());
+        pokelist.add(popmon.getSpDef());
+        pokelist.add(podmon.getSpDef());
+        
+        assertFalse(areAllIdentical(pokelist));
     }
     
     
@@ -410,26 +445,62 @@ public class PokeTest {
         podmon.setBST(45);
         podmon.distrStats();
         
-        assertTrue(
-            (pokmon.getSpd() != pokermon.getSpd()) ||
-            (pokmon.getSpd() != pokomon.getSpd()) ||
-            (pokmon.getSpd() != poqmon.getSpd()) ||
-            (pokmon.getSpd() != pocmon.getSpd()) ||
-            (pokmon.getSpd() != pokamon.getSpd()) ||
-            (pokmon.getSpd() != pokumon.getSpd()) ||
-            (pokmon.getSpd() != pokymon.getSpd()) ||
-            (pokmon.getSpd() != popmon.getSpd()) ||
-            (pokmon.getSpd() != podmon.getSpd())
-        );
+        //Add all of the generated SPD values to a list so that we can more easily compare them
+        ArrayList pokelist = new ArrayList ();
+        pokelist.add(pokmon.getSpd());
+        pokelist.add(pokermon.getSpd());
+        pokelist.add(pokomon.getSpd());
+        pokelist.add(poqmon.getSpd());
+        pokelist.add(pocmon.getSpd());
+        pokelist.add(pokamon.getSpd());
+        pokelist.add(pokumon.getSpd());
+        pokelist.add(pokymon.getSpd());
+        pokelist.add(popmon.getSpd());
+        pokelist.add(podmon.getSpd());
+        
+        assertFalse(areAllIdentical(pokelist));
     }
     
-    
+    //-----------------Typing Tests------------------
+    //Test that a valid typing is generated (at least a primary type)
     @Test
     public void typingTest() {
         TypeList types = TypeList.getInstance();
         Typing new_typing = types.genRandomTyping();
         
         assertTrue(new_typing.getPrimary() != "None");
+    }
+    
+    //Test that the primary type is not the same every time.
+    @Test
+    public void typingUniquenessTest() {
+        TypeList types = TypeList.getInstance();
+        Typing typing = types.genRandomTyping();
+        Typing typing_two = types.genRandomTyping();
+        Typing typing_three = types.genRandomTyping();
+        Typing typing_four = types.genRandomTyping();
+        Typing typing_five = types.genRandomTyping();
+        Typing typing_six = types.genRandomTyping();
+        Typing typing_seven = types.genRandomTyping();
+        Typing typing_eight = types.genRandomTyping();
+        Typing typing_nine = types.genRandomTyping();
+        Typing typing_ten = types.genRandomTyping();
+        
+        //Add all of the generated primary types to a list so that we can more easily compare them
+        ArrayList typing_list = new ArrayList ();
+        typing_list.add(typing.getPrimary());
+        typing_list.add(typing_two.getPrimary());
+        typing_list.add(typing_three.getPrimary());
+        typing_list.add(typing_four.getPrimary());
+        typing_list.add(typing_five.getPrimary());
+        typing_list.add(typing_six.getPrimary());
+        typing_list.add(typing_seven.getPrimary());
+        typing_list.add(typing_eight.getPrimary());
+        typing_list.add(typing_nine.getPrimary());
+        typing_list.add(typing_ten.getPrimary());
+        
+        assertFalse(areAllIdentical(typing_list));
+        
     }
     
 }//end of tests
